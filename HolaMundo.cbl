7c0024ca1e45bb71fe99f91ca1b816b886e23080
Freddy875 <60365437+Freddy875@users.noreply.github.com>
2020-07-16 20:43:25 -0500

    Create HolaMundo.cbl
    Nuestro primer Hola Mundo en Cobol con todas las sentencias que nos permite hacer cobol como una buena practica

---
+++ HolaMundo.cbl
@@ -0,0 +1,18 @@
+*Mi primer porgrama en Cobol*
+
+identification division.
+
+program-id. HolaMundo.
+
+enviroment division. 
+
+data division.
+
+procedure division. 
+
+  display "Hola Mundo".
+  
+  stop "pausa".
+
+stop run.
+  
